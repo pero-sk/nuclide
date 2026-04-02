4aa40e14235ef1f0ac584c658391955602679813
penguin <127430703+programming-artist-py@users.noreply.github.com>
2026-04-02 19:10:13 +0100

    added platinum atom

---
+++ src/test/java/com/penguin/nuclide/tests/ReactionMatcherTest.java
@@ -0,0 +1,150 @@
+package com.penguin.nuclide.tests;
+
+import com.penguin.nuclide.reaction.ReactionConditions;
+import com.penguin.nuclide.reaction.ReactionContext;
+import com.penguin.nuclide.reaction.ReactionDefinition;
+import com.penguin.nuclide.reaction.ReactionMatcher;
+import com.penguin.nuclide.reaction.ReactionParticipant;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReactionMatcherTest {
+
+    private static ReactionDefinition unconditionalReaction() {
+        return new ReactionDefinition(
+                "nuclide:water_formation",
+                "Water Formation",
+                List.of(
+                        new ReactionParticipant("molecules:hydrogen_gas", 2),
+                        new ReactionParticipant("molecules:oxygen_gas", 1)
+                ),
+                List.of(
+                        new ReactionParticipant("molecules:water", 2)
+                ),
+                new ReactionConditions(null, null, false, null),
+                40
+        );
+    }
+
+    private static ReactionDefinition heatedSparkReaction() {
+        return new ReactionDefinition(
+                "nuclide:ignition_test",
+                "Ignition Test",
+                List.of(
+                        new ReactionParticipant("molecules:hydrogen_gas", 2),
+                        new ReactionParticipant("molecules:oxygen_gas", 1)
+                ),
+                List.of(
+                        new ReactionParticipant("molecules:water", 2)
+                ),
+                new ReactionConditions(300.0, null, true, null),
+                40
+        );
+    }
+
+    private static ReactionDefinition catalystReaction() {
+        return new ReactionDefinition(
+                "nuclide:catalyst_test",
+                "Catalyst Test",
+                List.of(
+                        new ReactionParticipant("atoms:sodium", 1),
+                        new ReactionParticipant("atoms:chlorine", 1)
+                ),
+                List.of(
+                        new ReactionParticipant("molecules:sodium_chloride", 1)
+                ),
+                new ReactionConditions(null, null, false, "atoms:platinum"),
+                20
+        );
+    }
+
+    @Test
+    void matchesUnconditionalReaction() {
+        Map<String, Integer> available = Map.of(
+                "molecules:hydrogen_gas", 2,
+                "molecules:oxygen_gas", 1
+        );
+
+        assertTrue(ReactionMatcher.matches(unconditionalReaction(), available));
+    }
+
+    @Test
+    void failsWhenSpeciesMissing() {
+        Map<String, Integer> available = Map.of(
+                "molecules:hydrogen_gas", 2
+        );
+
+        assertFalse(ReactionMatcher.matches(unconditionalReaction(), available));
+    }
+
+    @Test
+    void failsWhenSpeciesInsufficient() {
+        Map<String, Integer> available = Map.of(
+                "molecules:hydrogen_gas", 1,
+                "molecules:oxygen_gas", 1
+        );
+
+        assertFalse(ReactionMatcher.matches(unconditionalReaction(), available));
+    }
+
+    @Test
+    void failsWhenBelowMinTemperature() {
+        Map<String, Integer> available = Map.of(
+                "molecules:hydrogen_gas", 2,
+                "molecules:oxygen_gas", 1
+        );
+
+        ReactionContext context = new ReactionContext(100.0, true);
+
+        assertFalse(ReactionMatcher.matches(heatedSparkReaction(), available, context));
+    }
+
+    @Test
+    void failsWhenSparkIsRequiredButMissing() {
+        Map<String, Integer> available = Map.of(
+                "molecules:hydrogen_gas", 2,
+                "molecules:oxygen_gas", 1
+        );
+
+        ReactionContext context = new ReactionContext(500.0, false);
+
+        assertFalse(ReactionMatcher.matches(heatedSparkReaction(), available, context));
+    }
+
+    @Test
+    void matchesWhenTemperatureAndSparkRequirementsAreMet() {
+        Map<String, Integer> available = Map.of(
+                "molecules:hydrogen_gas", 2,
+                "molecules:oxygen_gas", 1
+        );
+
+        ReactionContext context = new ReactionContext(500.0, true);
+
+        assertTrue(ReactionMatcher.matches(heatedSparkReaction(), available, context));
+    }
+
+    @Test
+    void failsWhenCatalystIsMissing() {
+        Map<String, Integer> available = Map.of(
+                "atoms:sodium", 1,
+                "atoms:chlorine", 1
+        );
+
+        assertFalse(ReactionMatcher.matches(catalystReaction(), available, ReactionContext.DEFAULT));
+    }
+
+    @Test
+    void matchesWhenCatalystIsPresent() {
+        Map<String, Integer> available = Map.of(
+                "atoms:sodium", 1,
+                "atoms:chlorine", 1,
+                "atoms:platinum", 1
+        );
+
+        assertTrue(ReactionMatcher.matches(catalystReaction(), available, ReactionContext.DEFAULT));
+    }
+}
